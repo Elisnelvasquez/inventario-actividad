fd005712f7b8191e4a91620c7a4a5ee8f1c0577a
Elisnelvasquez <vasquezvicunae@uvm.edu.ve>
2024-11-10 16:50:36 -0400

    cambios en el controlador de inventario

--- controllers/InventarioController.js
+++ controllers/InventarioController.js
@@ -1,59 +1,121 @@
 const db = require('../config/database');
 
 class InventarioController {
-    static list(req, res) {
-        db.query('SELECT * FROM inventario', (err, results) => {
-            if (err) throw err;
-            res.render('inventario/index', { productos: results });
-        });
-    }
+  static list(req, res) {
+    db.query("SELECT * FROM inventario", (err, results) => {
+      if (err) throw err;
+      res.render("inventario/index", { productos: results });
+    });
+  }
 
-    static createForm(req, res) {
-        res.render('inventario/create');
-    }
+  static createForm(req, res) {
+    res.render("inventario/create");
+  }
 
-    static create(req, res) {
-        const { nombre, descripcion, categoria, precio, cantidad, proveedor, fecha_ingreso, codigo_barras } = req.body;
-        const query = 'INSERT INTO inventario (nombre, descripcion, categoria, precio, cantidad, proveedor, fecha_ingreso, codigo_barras) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-        db.query(query, [nombre, descripcion, categoria, precio, cantidad, proveedor, fecha_ingreso, codigo_barras], (err) => {
-            if (err) throw err;
-            res.redirect('/inventario');
-        });
-    }
+  static create(req, res) {
+    const {
+      nombre,
+      descripcion,
+      categoria,
+      precio,
+      cantidad,
+      proveedor,
+      fecha_ingreso,
+      codigo_barras
+    } = req.body;
+    const query =
+      "INSERT INTO inventario (nombre, descripcion, categoria, precio, cantidad, proveedor, fecha_ingreso, codigo_barras) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
-    static editForm(req, res) {
-        const id = req.params.id;
-        db.query('SELECT * FROM inventario WHERE id = ?', [id], (err, results) => {
-            if (err) throw err;
-            res.render('inventario/edit', { producto: results[0] });
-        });
-    }
+    db.query(
+      query,
+      [
+        nombre,
+        descripcion,
+        categoria,
+        precio,
+        cantidad,
+        proveedor,
+        fecha_ingreso,
+        codigo_barras
+      ],
+      (err) => {
+        if (err) {
+          return res
+            .status(500)
+            .json({ message: "Error al agregar el producto" });
+        }
+        res.status(200).json({ message: "Producto agregado exitosamente" });
+      }
+    );
+  }
 
-    static edit(req, res) {
-        const id = req.params.id;
-        const { nombre, descripcion, categoria, precio, cantidad, proveedor, fecha_ingreso, codigo_barras } = req.body;
-        const query = 'UPDATE inventario SET nombre = ?, descripcion = ?, categoria = ?, precio = ?, cantidad = ?, proveedor = ?, fecha_ingreso = ?, codigo_barras = ? WHERE id = ?';
-        db.query(query, [nombre, descripcion, categoria, precio, cantidad, proveedor, fecha_ingreso, codigo_barras, id], (err) => {
-            if (err) throw err;
-            res.redirect('/inventario');
-        });
-    }
+  static editForm(req, res) {
+    const id = req.params.id;
+    db.query("SELECT * FROM inventario WHERE id = ?", [id], (err, results) => {
+      if (err) throw err;
+      res.render("inventario/edit", { producto: results[0] });
+    });
+  }
 
-    static delete(req, res) {
-        const id = req.params.id;
-        db.query('DELETE FROM inventario WHERE id = ?', [id], (err) => {
-            if (err) throw err;
-            res.redirect('/inventario');
-        });
-    }
+  static edit(req, res) {
+    const id = req.params.id;
+    const {
+      nombre,
+      descripcion,
+      categoria,
+      precio,
+      cantidad,
+      proveedor,
+      codigo_barras
+    } = req.body;
+    const query = `
+            UPDATE inventario 
+            SET nombre = ?, descripcion = ?, categoria = ?, precio = ?, cantidad = ?, proveedor = ?, codigo_barras = ? 
+            WHERE id = ?
+        `;
 
-    static details(req, res) {
-        const id = req.params.id;
-        db.query('SELECT * FROM inventario WHERE id = ?', [id], (err, results) => {
-            if (err) throw err;
-            res.render('inventario/details', { producto: results[0] });
-        });
-    }
+    db.query(
+      query,
+      [
+        nombre,
+        descripcion,
+        categoria,
+        precio,
+        cantidad,
+        proveedor,
+        codigo_barras,
+        id
+      ],
+      (err) => {
+        if (err) {
+          return res
+            .status(500)
+            .json({ message: "Error al actualizar el producto" });
+        }
+        res.status(200).json({ message: "Producto actualizado exitosamente" });
+      }
+    );
+  }
+
+  static delete(req, res) {
+    const id = req.params.id;
+    db.query("DELETE FROM inventario WHERE id = ?", [id], (err) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ message: "Error al eliminar un articulo" });
+      }
+      res.status(200).json({ message: "Articulo eliminado exitosamente" });
+    });
+  }
+
+  static details(req, res) {
+    const id = req.params.id;
+    db.query("SELECT * FROM inventario WHERE id = ?", [id], (err, results) => {
+      if (err) throw err;
+      res.render("inventario/details", { producto: results[0] });
+    });
+  }
 }
 
 module.exports = InventarioController;
